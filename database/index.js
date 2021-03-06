ee6173e0729a174398ea8f5ea47fca8ce641cbe9
Carolyn Tran <carolyntran953@gmail.com>
2021-03-06 17:56:55 -0600

    Implement createPhotosArr function

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/fec');
 
 let photoGallerySchema = mongoose.Schema({
   id: Number,
@@ -11,3 +11,23 @@ let photoGallerySchema = mongoose.Schema({
 });
 
 let PhotoGallery = mongoose.model('Photos', photoGallerySchema);
+
+const createPhotosArr = () => {
+  const photos = [];
+  let numPhotos = 0;
+  let filepath;
+  let thumbnailFile;
+  let fullsizeFile;
+  let id = 100;
+
+  while (numPhotos < 100) {
+    filepath = `https://picsum.photos/id/${id}`;
+    fullsizeFile = `${filepath}/600/400`;
+    thumbnailFile = `${filepath}/300/200`;
+    photos.push({ fullsize: fullsizeFile, thumbnail: thumbnailFile });
+    id += 1;
+    numPhotos += 1;
+  }
+
+  return photos;
+};
