e44616ff910cdc0a14b36bb67bf3928247835e27
Carolyn Tran <carolyntran953@gmail.com>
2021-03-05 23:22:32 -0600

    initial setup

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+
+app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
