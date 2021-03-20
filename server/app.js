9795e215e7a00f6334c6b93f3a2bbedc19aaba62
Carolyn Tran <carolyntran953@gmail.com>
2021-03-20 18:21:59 -0500

    move html file from dist folder into main client folder

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/:id', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 app.get('/:id/photos', (req, res) => {
