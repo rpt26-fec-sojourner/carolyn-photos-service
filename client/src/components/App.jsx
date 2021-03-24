d3f04d06f679f31bbf17a7949ede8d4e34283dbd
Carolyn Tran <carolyntran953@gmail.com>
2021-03-23 23:43:59 -0500

    install html-webpack-plugin, css-loader, style loader, edit webpack config, css now loading

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PhotoGallery from './PhotoGallery.jsx';
+import '../../style.css';
 
 const App = () => {
   return (
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+// const css = require('../style.css');
 
 ReactDOM.render(<App />, document.getElementById('photos'));
