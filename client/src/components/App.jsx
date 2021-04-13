7f47fe956d3110ed4031cf41cae4fed8363e2237
Carolyn Tran <carolyntran953@gmail.com>
2021-04-12 23:59:56 -0500

    undo React Suspense import

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import PhotoGallery from './PhotoGallery.jsx';
 import '../../styles/photogallery.css';
 import '../../styles/allphotos.css';
